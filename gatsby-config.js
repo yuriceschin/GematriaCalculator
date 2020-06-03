8f14c0d37f30450eec1307471e6d5ecf7bb70d24
Yuri Ceschin <yuriceschin@gmail.com>
2020-06-03 17:51:26 +0200

    Add ServiceWorkerUpdate for PWA and updated icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ siteMetadata: {
     description:
       "Gematria or gimatria is a system of assigning numerical value to a word or phrase. Hebrew Gematria calculator is a tool to help in this task. Moreover you can find the correspondences of the Liber Sepher Sephiroth sub figurâ D",
     url: "https://gematria.starryabode.com", // No trailing slash allowed!
-    image: "src/images/icon.png" // Path to your image you placed in the 'static' folder
+    image: "src/images/icon512.png" // Path to your image you placed in the 'static' folder
   },
 plugins: [
  	{
@@ -18,23 +18,22 @@ plugins: [
 	{
 	resolve: `gatsby-plugin-manifest`,
 	    options: {
-	        name: "TQ Gematria Calculator",
-	        short_name: "TQ Gematria",
+	        name: "Gematria Calculator",
+	        short_name: "Gematria Calculator",
 	        start_url: "/",
 	        background_color: "#fafbed",
 	        theme_color: "#fafbed",
-	        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-	        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
 	        display: "standalone",
-	        icon: "src/images/icon.png", // This path is relative to the root of the site.
+	        icon: "src/images/icon512.png", // This path is relative to the root of the site.
 	    },
 	},
 	'gatsby-plugin-offline',
 	{
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
+		workboxConfig: {
+			globPatterns: ['**/*']
+		 }
       },
     },
     `gatsby-transformer-csv`,
